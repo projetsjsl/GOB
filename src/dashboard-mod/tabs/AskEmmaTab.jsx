7ed7b1d6b955cedc97534c46e0beafd45f128757
projetsjsl <projetsjsl@gmail.com>
2025-11-29 14:50:19 -0500

    chore: prune unused dashboard-mod build files

--- src/dashboard-mod/tabs/AskEmmaTab.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AskEmmaTabComponent from '../../components/tabs/AskEmmaTab';
-
-export const AskEmmaTab = (props) => {
-    return <AskEmmaTabComponent {...props} />;
-};
-
-export default AskEmmaTab;
